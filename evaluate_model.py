028d630505866ed20c5ca46e25d7e371b8524082
Henrietta Holze <szr214@alumni.ku.dk>
2021-04-24 20:45:40 +0200

    evaluate final model

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,4 +1,7 @@
 import pickle 
+import glob
+from feature_construction import compute_features
+from matplotlib.pyplot import plt
 
 
 # 1) make bar plot with feature importances 
@@ -13,5 +16,26 @@ with open('pickle_model.pkl', 'rb') as file:
 
 # from sklearn import metrics
 
+
+filenames_train = glob.glob("data/training/crystal_structs/*.pdb")
+
+features_train = compute_features(filenames_train)
+features_train = features_train.sort_values('protIDs').reset_index(drop=True)
+
+solubility_train = pd.read_csv("data/training/crystal_structs/solubility_values.csv")
+solubility_train = solubility_train.sort_values('protein').reset_index(drop=True)
+
+print(features_train.columns)
 print(rfr.feature_importances_)
+
+
+
+
+# filenames_train = glob.glob("data/test/ecoli_modelled_structs/*.pdb")
+
+# features_train = compute_features(filenames)
+# features_train = features.sort_values('protIDs').reset_index(drop=True)
+
+# solubility_train = pd.read_csv("data/training/crystal_structs/solubility_values.csv")
+# solubility_train = solubility.sort_values('protein').reset_index(drop=True)
 
