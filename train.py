741c6da0245eea56c1a264c4543c6abbb8e5d684
Henrietta Holze <szr214@alumni.ku.dk>
2021-04-25 01:53:06 +0200

    cleaned file

--- train.py
+++ train.py
@@ -12,16 +12,10 @@ import pickle
 filenames = glob.glob("data/training/crystal_structs/*.pdb")
 
 features = compute_features(filenames)
-
-# features = np.load('features.npy')
-# features = pd.read_csv('features.csv')
 features = features.sort_values('protIDs').reset_index(drop=True)
 
 solubility = pd.read_csv("data/training/crystal_structs/solubility_values.csv")
 solubility = solubility.sort_values('protein').reset_index(drop=True)
-
-# merge solubility and features (not in the same order?)
-# merged = pd.merge(features, solubility, left_on='protIDs', right_on='protein')
 
 # save the proteins 
 proteins = solubility['protein'].values
@@ -29,28 +23,10 @@ proteins = solubility['protein'].values
 X = features.iloc[:, 1:].to_numpy()
 y = solubility.iloc[:, 1:].to_numpy()
 
-# XTrain, XTest, YTrain, YTest = train_test_split(
-#     X, y, test_size=0.33, random_state=42)
-
 rfr = RandomForestRegressor(n_estimators=1000)
 rfr.fit(X, np.ravel(y))
-# rfr_prediction = rfr.predict(XTest)
-
-# from sklearn import metrics
-
-# print(rfr.feature_importances_)
-
-# print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(YTest, rfr_prediction))
-# print('Mean Squared Error (MSE):', metrics.mean_squared_error(YTest, rfr_prediction))
-# print('Root Mean Squared Error (RMSE):', np.sqrt(metrics.mean_squared_error(YTest, rfr_prediction)))
-# mape = np.mean(np.abs((YTest - rfr_prediction) / np.abs(YTest)))
-# print('Mean Absolute Percentage Error (MAPE):', round(mape * 100, 2))
-# print('Accuracy:', round(100*(1 - mape), 2))
-
-
 
 # save model
-
 pkl_filename = "pickle_model.pkl"
 with open(pkl_filename, 'wb') as file:
     pickle.dump(rfr, file)
