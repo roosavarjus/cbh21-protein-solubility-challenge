8f59dfc3cfa432f6e58c164627b57b170def5dea
bqn767 <bqn767@alumni.ku.dk>
2021-04-25 02:35:44 +0200

    feature importance

--- feature_importance.py
+++ feature_importance.py
@@ -20,25 +20,16 @@ import matplotlib.pyplot as plt
 with open('pickle_model.pkl', 'rb') as file:
     rfr = pickle.load(file)
 
-# train the random forrest with training data
-
 filenames = glob.glob("data/training/crystal_structs/*.pdb")
-
 features = compute_features(filenames)
 features = features.sort_values('protIDs').reset_index(drop=True)
 X = features.iloc[:, 1:]
 feature_importances = rfr.feature_importances_
 feature_names = X.columns
-print(feature_importances)
-print(feature_names)
-print(len(feature_names))
-print(len(feature_importances))
-print(X)
-
 sorted_idx = rfr.feature_importances_.argsort()
-print(sorted_idx)
 
 plt.barh(feature_names[sorted_idx], feature_importances[sorted_idx])
 plt.title("Feature importance")
+plt.tight_layout()
 plt.savefig("Feature_importance.png")
 plt.show()
